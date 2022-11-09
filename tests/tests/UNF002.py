947d158d9005c9eafc4328d409e1116fdcfe8fb0
Gideon <87426140+GideonBear@users.noreply.github.com>
2022-11-09 09:52:16 +0100

    Add test for `UNF002`

---
+++ tests/tests/UNF002.py
@@ -0,0 +1,12 @@
+raise NotImplementedError  # 7: UNF002: Reference to `NotImplementedError` found
+raise NotImplementedError()  # 7: UNF002: Reference to `NotImplementedError` found
+raise NotimplementedError
+raise ValueError
+NotImplementedError  # 1: UNF002: Reference to `NotImplementedError` found
+NotImplementedError()  # 1: UNF002: Reference to `NotImplementedError` found
+NotImplemented
+NotimplementedError
+raise NotImplemented
+raise NotImplemented()
+ValueError
+raise
